5d3c5f3d17141c952b3b36c9d36fba3d335b0528
Rahhulkumar25 <rahulkumar773954@gmail.com>
2025-09-26 15:37:25 +0530

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function login(){
     const username = document.getElementById("username").value.trim();
-    const passsword = document.getElementById("password").value.trim();
+    const password = document.getElementById("password").value.trim();
 
 
-    if(username === "Rahul08" && passsword === "Rahul620#"){
+    if(username === "Rahul08" && password === "Rahul620#"){
             alert("login successful");}
     else {
-        alert("Invaild username and password");
+        alert("Invalid username and password");
     }    
 }
